2ace724d0e27cf669afa3ecc110ab924192fb2bd
arefk <arefkhademi8@gmail.com>
2024-03-18 20:51:30 +0100

    First ever, Start of project!

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def print_hi(name):
+
+    print("Hi!!! Start of project 18/03/2024," , name)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Aref Najafabadi 196899')
+
